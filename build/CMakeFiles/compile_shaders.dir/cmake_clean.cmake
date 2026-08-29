d88cab87b980f55051d41b5c2c13b99b2ff368e1
Luis Valiñas <luisvc1126@gmail.com>
2026-08-29 16:34:27 +0200

    Delete build directory
    Deleted the unnecessary build directory

--- build/CMakeFiles/compile_shaders.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/compile_shaders"
-  "shader.spv"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/compile_shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
